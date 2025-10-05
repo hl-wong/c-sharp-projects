b9ecbe9b3360336aeb94caba16bbd7aa14ef7408
HL WONG <devhlwong@gmail.com>
2025-10-05 11:37:08 +0800

    To-Do List

---
+++ To-Do List/To-Do List/Program.cs
@@ -0,0 +1,85 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Collections;
+
+namespace To_Do_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Task> tasks = new List<Task>();
+
+            bool exit = false;
+
+            while (!exit)
+            {
+                Run();
+
+                try
+                {
+                    Console.Write("\nPlease enter the choice: ");
+                    int choice = Convert.ToInt32(Console.ReadLine());
+
+                    Task task;
+
+                    switch (choice)
+                    {
+                        case 1:
+                            // Add Task
+                            Console.Write("\nPlease enter the task name: ");
+                            string task_name = Console.ReadLine().Trim();
+
+                            if (task_name != "")
+                            {
+                                task = new Task(task_name, false, tasks);
+                                task.AddTask(task);
+                            }
+                            else
+                            {
+                                Console.WriteLine("Task name cannot be empty.\n");
+                            }
+                            break;
+                        case 2:
+                            // Update Task
+                            task = new Task(tasks);
+                            task.UpdateTask();
+                            break;
+                        case 3:
+                            // Delete Task
+                            task = new Task(tasks);
+                            task.DeleteTask();
+                            break;
+                        case 4:
+                            // View Tasks
+                            task = new Task(tasks);
+                            task.ViewTasks();
+                            break;
+                        case 5:
+                            exit = true;
+                            Console.WriteLine("Exit program.");
+                            break;
+                        default:
+                            Console.WriteLine("Invalid choice.\n");
+                            break;
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Please enter a number.\n");
+                }
+            }
+
+            static void Run()
+            {
+                Console.WriteLine("To-Do List");
+                Console.WriteLine("==========");
+                Console.WriteLine("[1] Add Task");
+                Console.WriteLine("[2] Update Task");
+                Console.WriteLine("[3] Delete Task");
+                Console.WriteLine("[4] View Tasks");
+                Console.WriteLine("[5] Exit");
+            }
+        }
+    }
+}
---
+++ To-Do List/To-Do List/Task.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace To_Do_List
+{
+    class Task
+    {
+        private string name;
+        private bool status;
+        private List<Task> tasks;
+
+        public Task(string name, bool status, List<Task> tasks)
+        {
+            this.name = name;
+            this.status = status;
+            this.tasks = tasks;
+        }
+
+        public Task(List<Task> tasks)
+        {
+            this.tasks = tasks;
+        }
+
+        public string Name
+        {
+            get { return name; } set { name = value; }
+        }
+
+        public bool Status
+        {
+            get { return status; } set { status = value; }
+        }
+
+        public List<Task> Tasks
+        {
+            get { return tasks; } set { tasks = value; }
+        }
+
+        public void AddTask(Task task)
+        {
+            tasks.Add(task);
+            Console.WriteLine("Task have been added to the List.\n");
+        }
+
+        public void UpdateTask()
+        {
+            if (!CheckTaskSize())
+            {
+                Console.WriteLine("There are no task yet.\n");
+            }
+            else
+            {
+                DisplayTasks();
+
+                try
+                {
+                    Console.Write("Please enter the choice to update task: ");
+                    int choice = Convert.ToInt32(Console.ReadLine());
+                    if (choice > 0 && choice <= tasks.Count)
+                    {
+                        Console.Write("\nPlease enter the task name (optional): ");
+                        string task_name = Console.ReadLine().Trim();
+
+                        // if task_name is empty, automatically assign the existing value to task_name instead of empty.
+                        if (task_name == "")
+                        {
+                            task_name = tasks[choice - 1].Name;
+                        }
+
+                        Console.WriteLine("\nTask Status");
+                        Console.WriteLine("===========");
+                        Console.WriteLine("[1] Incomplete");
+                        Console.WriteLine("[2] Complete");
+
+                        try
+                        {
+                            Console.Write("Please update the task status: ");
+                            int task_status = Convert.ToInt32(Console.ReadLine());
+
+                            Task task;
+
+                            switch (task_status)
+                            {
+                                case 1:
+                                    task = new Task(task_name, false, tasks);
+                                    tasks[choice - 1] = task;
+                                    break;
+                                case 2:
+                                    task = new Task(task_name, true, tasks);
+                                    tasks[choice - 1] = task;
+                                    break;
+                                default:
+                                    Console.WriteLine("Invalid choice.");
+                                    break;
+                            }
+                            Console.WriteLine("Task have been updated to the List.\n");
+                        }
+                        catch
+                        {
+                            Console.WriteLine("Please enter a number.\n");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid choice.\n");
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Please enter a number.\n");
+                }
+            }
+        }
+
+        public void DeleteTask()
+        {
+            if (!CheckTaskSize())
+            {
+                Console.WriteLine("There are no task yet.\n");
+            }
+            else
+            {
+                DisplayTasks();
+                
+                try
+                {
+                    Console.Write("Please enter the choice to delete task: ");
+                    int choice = Convert.ToInt32(Console.ReadLine());
+                    if (choice > 0 && choice <= tasks.Count)
+                    {
+                        tasks.Remove(tasks[choice - 1]);
+                        Console.WriteLine("Task have been deleted from the List.\n");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid choice.\n");
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Please enter a number.\n");
+                }
+            }
+        }
+
+        public void ViewTasks()
+        {
+            if (!CheckTaskSize())
+            {
+                Console.WriteLine("There are no task yet.\n");
+            }
+            else
+            {
+                DisplayTasks();
+            }
+        }
+
+        public bool CheckTaskSize()
+        {
+            if (tasks.Count == 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public void DisplayTasks()
+        {
+            Console.WriteLine("\nTasks");
+            Console.WriteLine("=====");
+            for (int i = 0; i < tasks.Count; i++)
+            {
+                if (tasks[i].Status == true)
+                {
+                    Console.WriteLine("[" + (i + 1) + "] " + tasks[i].Name + " / Complete");
+                }
+                else
+                {
+                    Console.WriteLine("[" + (i + 1) + "] " + tasks[i].Name + " / Incomplete");
+                }
+
+                if (i == tasks.Count - 1) { Console.WriteLine("\n"); }
+            }
+        }
+    }
+}
